9f85003af376687c1e92b6eb7e18c54c8ead8c58
dbono1 <dbono1@fordham.edu>
2020-10-15 08:58:29 -0400

    midterm

--- assets/js/recent-work.js
+++ assets/js/recent-work.js
@@ -2,7 +2,8 @@ var work = [
     {"title":"Comics", "slug":"week02", "link":"/hw2/hw2.html", "date":"2020-09-10"},
     {"title":"Week 04", "slug":"week04", "link":"/hw4/hw4.html", "date":"2020-09-20"},
     {"title":"Slideshow", "slug":"week04", "link":"/slideshow/project.html", "date":"2020-09-20"},
-    {"title":"The Crito", "slug":"week05", "link":"/hw5-netart/hw5-start.html", "date":"2020-10-10"}
+    {"title":"The Crito", "slug":"week05", "link":"/hw5-netart/hw5-start.html", "date":"2020-10-10"},
+    {"title":"MidtermProposal", "slug":"midterm", "link":"/midterm/proposal/proposal.html", "date":"2020-10-15"}
 ]
 
 function setRecentWork() {
