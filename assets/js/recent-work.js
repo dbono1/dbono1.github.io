dcbc7bedfa56f64416e8121826cb77903f3546f9
dbono1 <dbono1@fordham.edu>
2020-11-09 17:22:07 -0500

    the button

--- assets/js/recent-work.js
+++ assets/js/recent-work.js
@@ -3,7 +3,9 @@ var work = [
     {"title":"Week 04", "slug":"week04", "link":"/hw4/hw4.html", "date":"2020-09-20"},
     {"title":"Slideshow", "slug":"week04", "link":"/slideshow/project.html", "date":"2020-09-20"},
     {"title":"The Crito", "slug":"week05", "link":"/hw5-netart/hw5-start.html", "date":"2020-10-10"},
-    {"title":"Midterm Prop", "slug":"midterm", "link":"/midterm/proposal/proposal.html", "date":"2020-10-15"}
+    {"title":"Midterm Prop", "slug":"midterm", "link":"/midterm/proposal/proposal.html", "date":"2020-10-15"},
+    {"title":"Midterm", "slug":"midterm", "link":"/midterm/site/home", "date":"2020-10-20"},
+    {"title":"TheButton", "slug":"midterm", "link":"/button", "date":"2020-10-20"}
 ]
 
 function setRecentWork() {
