0606995eaac9b546fc96b008fd83f87f581d9c67
dbono1 <dbono1@fordham.edu>
2020-12-08 13:38:41 -0500

    DONE

--- assets/js/recent-work.js
+++ assets/js/recent-work.js
@@ -5,26 +5,47 @@ var work = [
     {"title":"The Crito", "slug":"week05", "link":"/hw5-netart/hw5-start.html", "date":"2020-10-10"},
     {"title":"Midterm Prop", "slug":"midterm", "link":"/midterm/proposal/proposal.html", "date":"2020-10-15"},
     {"title":"Midterm", "slug":"midterm", "link":"/midterm/site/home", "date":"2020-10-20"},
-    {"title":"TheButton", "slug":"donotpress", "link":"/button", "date":"2020-10-20"}
+    {"title":"TheButton", "slug":"donotpress", "link":"/button", "date":"2020-10-20"},
+    {"title":"Flappy", "slug":"flappy", "link":"/flappy", "date":"2020-10-20"},
+    {"title":"Final Project", "slug":"italian-american-virtual-museum", "link":"/italian-american-virtual-museum", "date":"2020-10-20"}
 ]
 
-function setRecentWork() {
+let F = document.createElement('a')
+console.log(F)
+
+//function setRecentWork() {
     var HTMLwork = document.getElementById('class-work');
     //This is a template dom object:
-    
-    for(i = 0; i < work.length; i++) {
-    
-        var dom = "<a id='%slug' href='%link'>%title</a>";
-        var json_element = work[i];
-        console.log(json_element);
+    for(let i = 0; i < work.length; i++) {
+        //var dom = "<a id='%slug' href='%link'>%title</a>";
+        let json_element = work[i];
+        
+        
+        let a =document.createElement('a');
+
+        a.id = json_element["slug"];
+        a.href = json_element["link"];
+        a.innerHTML = json_element["title"];
+        let slug = json_element["slug"];
+        console.log("linear-gradient(rgba(0,0,0,0), #000), url(/assets/img/" + slug + ".jpg);")
+        a.style.background = "linear-gradient(rgba(0,0,0,0), #000), url(/assets/img/" + slug + ".jpg)";
+        a.style.backgroundSize = "cover";
+        a.style.backgroundPosition = "center";
+        console.log(a)
+        HTMLwork.appendChild(a);
+        /*
         Object.keys(json_element).forEach(function(k){
             console.log(k + ' - ' + json_element[k]);
             dom = dom.replace('%' + k, json_element[k]);
         });
-        HTMLwork.innerHTML += "\n" + dom;
+        HTMLwork.appendChild(createEleme)
 
-        let slug = json_element["slug"];
-        document.querySelector("#"+ String(slug)).style.background = "background: linear-gradient(rgba(0,0,0,0), #000), url(/assets/img/" + slug + ".jpg);";
-
+        
+        
+        document.querySelector("#"+ String(slug)).style.background = "linear-gradient(rgba(0,0,0,0), #000), url(/assets/img/" + slug + ".jpg);";
+        */
     }
-}
+//}
+
+//setRecentWork();
+
--- italian-american-virtual-museum/assets/js/nav.js
+++ italian-american-virtual-museum/assets/js/nav.js
@@ -1,9 +1,9 @@
 let nav_contents = {
     "Home": [],
     "Historical Events": [
-        "Mass Migration 1890-1920",
-        "New Orleans Lynching",
-        "World War II"
+        /*"Mass Migration 1890-1920",*/
+        "New Orleans Lynching"
+        /*"World War II"*/
     ],
     "People": [
         "Frank Sinatra"
@@ -11,12 +11,10 @@ let nav_contents = {
 }
 
 let info_content = {
-    "mass-migration-1890-1920": ["ellis-island.png", "From 1880 to 1915, 13 million Italians migrated out of Italy, making Italy the scene of the largest voluntary emigration in recorded world history.<a href='https://en.wikipedia.org/wiki/Italian_Americans#The_period_of_mass_immigration_(1890%E2%80%931920)'>wiki</a>"],
+    //"mass-migration-1890-1920": ["ellis-island.png", "From 1880 to 1915, 13 million Italians migrated out of Italy, making Italy the scene of the largest voluntary emigration in recorded world history.<a href='https://en.wikipedia.org/wiki/Italian_Americans#The_period_of_mass_immigration_(1890%E2%80%931920)'>wiki</a>"],
     "new-orleans-lynching": ["lynching.jpg", "The March 14, 1891, New Orleans lynchings were the murders of 11 Italian Americans in New Orleans, Louisiana, by a mob for their alleged role in the murder of city police chief David Hennessy after some of them had been acquitted at trial. It was one of the largest single mass lynchings in American history. <a href='https://en.wikipedia.org/wiki/March_14,_1891_New_Orleans_lynchings'>wiki</a>"],
-    "world-war-ii": ["wwii.jpg", "As a member of the Axis powers, Italy declared war on the United States in December 1941 after Japan attacked Pearl Harbor. Over 1.5 million Italian-Americans served in the armed forces during World War II, amounting to some 10% of American soldiers in total. <a href='https://en.wikipedia.org/wiki/Italian_Americans#World_War_II'>wiki</a>"],
+    //"world-war-ii": ["wwii.jpg", "As a member of the Axis powers, Italy declared war on the United States in December 1941 after Japan attacked Pearl Harbor. Over 1.5 million Italian-Americans served in the armed forces during World War II, amounting to some 10% of American soldiers in total. <a href='https://en.wikipedia.org/wiki/Italian_Americans#World_War_II'>wiki</a>"],
     "frank-sinatra": ["sinatra.jpg", "Francis Albert Sinatra (/sɪˈnɑːtrə/; December 12, 1915 – May 14, 1998) was an American singer, actor and producer who was one of the most popular and influential musical artists of the 20th century. He is one of the best-selling music artists of all time, having sold more than 150 million records worldwide. <a href='https://en.wikipedia.org/wiki/Frank_Sinatra'>wiki</a>"]
-    
-
 }
 
 //The below code renders the navbar from the 
